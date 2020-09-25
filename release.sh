df7211adddad4b3584fa0cae72e0122d9dd536be
Duncan McClean <duncan@steadfastcollective.com>
2020-09-25 14:26:48 +0100

    Release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+echo "Release Name:"
+read releaseName
+
+rm -rf builds/fabric
+php fabric app:build fabric --build-version=$releaseName
+
+git add builds/fabric
+git commit -m "Build: $releaseName"
+git push origin master
+
+git tag -a $releaseName -m $releaseName
+git push --tags
+
+echo "Application built and tagged!"
+echo "Remember to create release with changelog."

b977b42f309dfa772a2c2460d5acc1c747d80415
Duncan McClean <duncan@steadfastcollective.com>
2020-10-30 15:03:01 +0000

    Set the app env explicitly for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,17 @@
 
 namespace SteadfastCollective\Fabric\Tests;
 
+use Illuminate\Support\Facades\Config;
 use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Config::set('app.env', 'testing');
+    }
 }
